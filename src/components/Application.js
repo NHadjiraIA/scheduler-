8163d08d1f3c78cad02b45904bdcc249d04cd746
NHadjiraIA <bkh.hadjira@gmail.com>
2021-08-05 15:28:08 -0400

    Retrieving Appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,10 +1,51 @@
 import React, {useState, useEffect} from "react";
 import axios from 'axios';
 import DayList from './DayList';
+import Appointment from './Appointment'
 
 import "components/Application.scss";
 
 export default function Application(props) {
+  const appointments = [
+    {
+      id: 1,
+      time: "12pm",
+    },
+    {
+      id: 2,
+      time: "1pm",
+      interview: {
+        student: "Lydia Miller-Jones",
+        interviewer: {
+          id: 1,
+          name: "Sylvia Palmer",
+          avatar: "https://i.imgur.com/LpaY82x.png",
+        }
+      }
+    },
+    {
+      id: 3,
+      time: "2pm",
+      interview: {
+        student: "Archie Cohen",
+        interviewer: {
+          id: 2,
+          name: "Sylvia Palmer",
+          avatar: "https://i.imgur.com/LpaY82x.png",
+        }
+      }
+    }
+  ];
+  const appointmentData = appointments.map(data =>{
+    return(
+      <Appointment
+      key={data.id} 
+      id={data.id} 
+      time={data.time} 
+      interview={data.interview} 
+      />
+    );
+  })
   const [days, setDays] = useState([]);
 
   useEffect(() => {
@@ -14,6 +55,7 @@ export default function Application(props) {
       console.log(response.data);
     });
   }, [])
+ 
 
   return (
     <main className="layout">
@@ -37,6 +79,10 @@ export default function Application(props) {
         {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
       </section>
       <section className="schedule">
+      
+          {appointmentData}
+      
+         
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
       </section>
     </main>
