e483568567d279f7a44c54f8bd1ecc58f3c996a4
NHadjiraIA <bkh.hadjira@gmail.com>
2021-08-05 14:59:25 -0400

    request days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
+import DayList from './DayList';
 
 import "components/Application.scss";
 
 export default function Application(props) {
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    const testURL = 'http://localhost:8001/api/days';
+    axios.get(testURL).then(response => {
+      setDays(response.data)
+      console.log(response.data);
+    });
+  }, [])
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -13,12 +25,15 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu"></nav>
+        <DayList
+         days={days}
+         />
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
           alt="Lighthouse Labs"
         />
-
+        
         {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
       </section>
       <section className="schedule">
